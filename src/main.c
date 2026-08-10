41a0be6416fed706076f1772876c8a772903577d
mortadelan <mortadela11223344@gmail.com>
2026-08-09 21:07:30 -0300

    change main.c to use new functions

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "lib.c"
 #include <math.h>
+#include "lib.c"
 
 int main(void) {
   int mod;
@@ -11,6 +11,11 @@ int main(void) {
   calc_pow5_residues(fifth, mod);
   populate_grid(mod, grid, fifth);
   print_grid(mod, grid);
+
+  int row_width = ((((digits(mod) + 1) * 3) + 1) * mod) + 1;
+  char img[mod + 3][row_width];
+  populate_header(mod, row_width, img);
+  grid_to_img(mod, row_width, img, grid);
 
   return 0;
 }
